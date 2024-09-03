7f9afadd5f3f63aceb0812bcfceaf3e69f79ff05
julianBogado1 <jbogado@itba.edu.ar>
2024-09-03 15:45:08 -0300

    chore: actually a fix but lets see

--- release.config.js
+++ release.config.js
@@ -1,14 +1,12 @@
 const config = {
-  branches: ['main'],    //1
+  branches: ['main'],
   plugins: [
-    '@semantic-release/commit-analyzer',    //2
-    '@semantic-release/release-notes-generator',  //3
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
     ["@semantic-release/git", {
       "assets": ["dist/*.js", "dist/*.js.map"],
-      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"  //4
+      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
     }],
-    '@semantic-release/github'  //5
+    '@semantic-release/github'
   ]
 };
-
-module.exports = config;
